e98d68f1393b4f372a101fee674d25d31f102801
csaravia <csaravia@csr>
2025-11-29 21:05:44 +0000

    Se agrega boton logout en la lista de digimones. Para volver a iniciar la aplicacion desde login

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,17 +4,39 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title> Digimon App </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn flat round dense icon="logout" @click="handleLogout">
+          <q-tooltip>Cerrar Sesión</q-tooltip>
+        </q-btn>
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+        <q-item-label header> Menú Principal </q-item-label>
 
-        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
+        <q-item clickable @click="goToHome">
+          <q-item-section avatar>
+            <q-icon name="home" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Inicio</q-item-label>
+            <q-item-label caption>Galería de Digimones</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-separator />
+
+        <q-item clickable @click="handleLogout">
+          <q-item-section avatar>
+            <q-icon name="logout" color="negative" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Cerrar Sesión</q-item-label>
+            <q-item-label caption>Volver al login</q-item-label>
+          </q-item-section>
+        </q-item>
       </q-list>
     </q-drawer>
 
@@ -26,56 +48,46 @@
 
 <script setup>
 import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { useRouter } from 'vue-router'
+import { Notify, Dialog } from 'quasar'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
-]
-
+const router = useRouter()
 const leftDrawerOpen = ref(false)
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
+}
+
+function goToHome() {
+  router.push('/home')
+  leftDrawerOpen.value = false
+}
+
+function handleLogout() {
+  Dialog.create({
+    title: 'Cerrar Sesión',
+    message: '¿Estás seguro de que deseas cerrar sesión?',
+    cancel: {
+      label: 'Cancelar',
+      flat: true,
+    },
+    ok: {
+      label: 'Cerrar Sesión',
+      color: 'negative',
+    },
+  }).onOk(() => {
+    // Limpiar localStorage
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+
+    Notify.create({
+      type: 'info',
+      message: 'Sesión cerrada correctamente',
+      position: 'top',
+    })
+
+    // Redirigir al login
+    router.push('/')
+  })
 }
 </script>
